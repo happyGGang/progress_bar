e8222130b8cd2167a7445fa989cbc04a673b6560
joy.gang <joy.gang@dxworks.kr>
2022-09-24 02:59:10 +0900

    [add] throttle 추가

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 ;(function () {
   'use strict'
 
+  let timerId
   const get = (target) => {
     return document.querySelector(target)
   }
 
+  const throttle = (callback, time) => {
+    if(timerId) return
+    timerId = setTimeout(() => {
+      callback()
+      timerId = undefined
+    }, time)
+  }
+
   const $progressBar = get('.progress-bar')
 
-  const onscroll = () => {
+  const onScroll = () => {
     const height = 
     document.documentElement.scrollHeight - 
     document.documentElement.clientHeight
@@ -18,5 +27,5 @@
     $progressBar.style.width = width + '%'
   }
 
-  window.addEventListener('scroll', () => onscroll()) 
+  window.addEventListener('scroll', () => throttle(onScroll, 100)) 
 })()
